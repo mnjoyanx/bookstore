dbf98989d71769bdc5af047b4a18298cad0b70bb
a-eliza <eliza.aleksanyan94@gmail.com>
2021-03-24 18:27:21 +0400

    product style

--- components/products/Products.js
+++ components/products/Products.js
@@ -1,28 +1,28 @@
 class Products {
 
-    
-  render() {
-    let catalogHtml = "";
 
-    CATALOG.map(({ name, imgSrc, price }) => {
-      catalogHtml += `
-                <li class="hello">
-                    <span>${name}</span>
+    render() {
+        let catalogHtml = "";
+
+        CATALOG.map(({ name, imgSrc, price }) => {
+            catalogHtml += `
+                <li class="products__list-main">
+                    <p>${name}</p>
                     <img src="${imgSrc}" />
                     <span>${price}</span>
-                    <button>Add to Card</button>
+                    <button class="button-primary">Add to Card</button>
                 </li>
             `;
-    });
+        });
 
-    const catalogList = `
-            <ul>
+        const catalogList = `
+            <ul class="products__items-main">
                 ${catalogHtml}
             </ul>
         `;
 
-    rootProducts.innerHTML = catalogList;
-  }
+        rootProducts.innerHTML = catalogList;
+    }
 }
 
 const products = new Products();
